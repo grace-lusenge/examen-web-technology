407b1f43a29da629ef508fb9b24100f210c1a328
Ndotoli <vndotoli@gmail.com>
2024-09-30 17:58:10 +0200

    Notre tache consiste à entrer,modifier et supprimer les entrées

--- views/entree-produit.php
+++ views/entree-produit.php
@@ -1,6 +1,15 @@
 <?php
-    ob_start(); 
+    ob_start();
+    include('../upload/connexion.php');
     $titre = "Gestion des Entrées des produits";
+    $database = new Connexion();
+    $con = $database->get_connexion();
+    $req = $con->prepare("SELECT entree_produit.id,produit.description, entree_produit.date AS date, entree_produit.id_produit AS produit, entree_produit.quantite AS quantite, entree_produit.prix_achat AS prix_achat,entree_produit.prix_vente AS prix_vente, entree_produit.date_expiration AS date_expiration FROM entree_produit JOIN produit ON entree_produit.id_produit=produit.id ");
+
+    $req1 = $con->prepare("SELECT * FROM produit");
+    $req->execute();
+    $req1->execute();
+
 ?>
 
     <div class="modal fade" id="addModal" tabindex="-1" role="dialog" aria-labelledby="addModalLabel" aria-hidden="true">
@@ -13,34 +22,41 @@
                     </button>
                 </div>
                 <div class="modal-body">
-                    <form id="Form" method="post">
+                    <form action="../upload/upload-entree-produit.php",id="Form" method="post">
+                        <input type="text" name="idUp" id="idUp" hidden>
                         <div class="form-group">
                             <label for="quantite">Produits</label>
-                            <select  id="produit" class="form-control">
-                                <option value="">Option1</option>
-                                <option value="">Option2</option>
+
+                            <select name="produit" id="produit" class="form-control">
+                                <?php
+                                    while($row = $req1->fetch()){
+                                        ?>
+                                         <option value="<?=$row->id?>"><?=$row->description?></option>
+                                   <?php }?>
+                                
                             </select>
                         </div>
                         <div class="form-group">
                             <label for="quantite">Quantité</label>
-                            <input type="number" class="form-control" id="quantite"  required>
+                            <input name="quantite" type="number" class="form-control" id="quantite"  required>
                         </div>
                         <div class="form-group">
                             <label for="prix_achat">Prix d'achat</label>
-                            <input type="number" class="form-control" id="prix_achat"  required>
+                            <input name="prix_achat" type="number" class="form-control" id="prix_achat"  required>
                         </div>
                         <div class="form-group">
                             <label for="prix_vente">Prix de Vente</label>
-                            <input type="number" class="form-control" id="prix_vente"  required>
+                            <input name="prix_vente" type="number" class="form-control" id="prix_vente"  required>
                         </div>
 
                         <div class="form-group">
-                            <label for="prix_expiration">Date d'expiration</label>
-                            <input type="date" class="form-control" id="date_expriration"  required>
+                            <label for="date_expiration">Date d'expiration</label>
+                            <input name="date_expiration" type="date" class="form-control" id="date_expriration"  required>
                         </div>
 
 
-                        <button type="submit" class="btn btn-primary">Enregistrer</button>
+                        <input type="submit" name="submit"class=" btn btn-primary" value="Enregistrer">
+                        <!-- <button type="submit" class="btn btn-primary">Enregistrer</button> -->
                     </form>
                 </div>
             </div>
@@ -52,7 +68,7 @@
   <div class="modal-dialog">
     <div class="modal-content">
       <div class="modal-header">
-        <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+        <h1 class="modal-title fs-5" id="staticBackdropLabel">Suppression</h1>
         <button type="button" class="close" data-dismiss="modal" aria-label="Close">
             <span aria-hidden="true">×</span>
         </button>
@@ -62,13 +78,13 @@
                 est important de noter que cette action est définitive et ne peut être révoquée.</p>
         </div>
       <div class="modal-footer">
-        <form action="" method="post">
+        <form action="../upload/upload-entree-produit.php" method="post">
             <input type="text" id="idDel" name="idDel" hidden>
             <button type="button" class="btn btn-secondary" data-dismiss="modal" aria-label="Close">Non</button>
             <input type="submit" class="btn btn-primary" value="Oui">
         </form>
         
-      </div>
+      </div>2
     </div>
   </div>
 </div>
@@ -89,28 +105,40 @@
         <table class="table table-striped mt-3">
             <thead>
                 <tr>
-                    <th>Numero</th>
-                    <th>Description</th>
+                    <th>Id</th>
+                    <th>Date</th>
+                    <th>produit</th>
+                    <th>Quantité</th>
+                    <th>Prix d'achat</th>
+                    <th>Prix de vente</th>
+                    <th>Date d'expiration</th>
                     <th>Actions</th>
                 </tr>
             </thead>
             <tbody>
-                <tr>
-                    <td>1</td>
-                    <td>Description1</td>
-                    <td>
-                        <button data-toggle="modal" data-target="#addModal" class="btn btn-warning btn-sm editbtn" id="editbtn">Mod</button>
-                         <a class="btn btn-danger btn-sm deletebtn" data-toggle="modal" data-target="#modalSuppression">sup</a>
-                    </td>
-                </tr>
-                <tr>
-                    <td>2</td>
-                    <td>Description2</td>
-                    <td>
-                        <button data-toggle="modal" data-target="#addModal" class="btn btn-warning btn-sm editbtn" id="editbtn">Mod</button>
-                         <a class="btn btn-danger btn-sm deletebtn" data-toggle="modal" data-target="#modalSuppression">sup</a>
-                    </td>
-                </tr>
+                <?php 
+                    while ($data=$req->fetch()){
+                        ?>
+                            <tr>
+                                <td><?=$data->id ?></td>
+                                <td><?=$data->date ?></td>
+                                <td><?=$data->description ?></td>
+                                <td><?=$data->quantite ?></td>
+                                <td><?=$data->prix_achat ?></td>
+                                <td><?=$data->prix_vente ?></td>
+                                <td><?=$data->date_expiration ?></td>
+                                <td>
+                                    <button data-toggle="modal" data-target="#addModal" class="btn btn-warning btn-sm editbtn" id="editbtn">Mod</button>
+                                    <a class="btn btn-danger btn-sm deletebtn" data-toggle="modal" data-target="#modalSuppression">sup</a>
+                                </td>
+                                
+                            </tr>
+                        <?php
+                    }
+                
+
+                ?>
+                
             </tbody>
         </table>
        
@@ -127,7 +155,15 @@
         $(".editbtn").on("click", function() {
             $tr = $(this).closest('tr');var data = $tr.children('td').map(function(){return $(this).text();}).get();
 
-            $('#votre id ici').val(data[0]);
+            $('#idUp').val(data[0]);
+            $('#id').val(data[0]);
+            $('#id_produit').val(data[1]);
+            $('#date').val(data[2]);
+            $('#quantite').val(data[3]);
+            $('#prix_achat').val(data[4]);
+            $('#prix_vente').val(data[5]);
+            $('#date_expiration').val(data[6]);
+
            
         });
         $(".deletebtn").on("click", function() {
